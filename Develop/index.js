f3ed11fc175478fb0087c324e7b0ccf6c4c1ddf5
andybockwrites <andybockwrites@gmail.com>
2023-03-03 01:43:27 -0500

    most of the way there...

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,123 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project Title?',
+        validate: (value) => { if (value) { return true } else { return `Please name you project.` } },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your product description?',
+        validate: (value) => { if (value) { return true } else { return `Please describe your project.` } },
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What did you do to install your project? What steps did you take?',
+        validate: (value) => { if (value) { return true } else { return `Please explain your installation process.` } },
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How would you describe the usage? Provide instructions.',
+        validate: (value) => { if (value) { return true } else { return `Please instructions for how to use your project.` } },
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please list your collaborators and/or source code. If none, type N/A.',
+        validate: (value) => { if (value) { return true } else { return `Please provide a list, or type N/A.` } },
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Have you developed any tests for your application? Can you provide any examples? If none, type N/A',
+        validate: (value) => { if (value) { return true } else { return `Please provide any tests for your application, or type N/A.` } },
+    },
+];
+
+function generateReadMe() {
+    const inputData =
+    `
+        # Title
+        # ${questions.title}
+
+        ## Description
+
+        ${questions.description}
+
+        ## Table of Contents
+
+        1. [Installation] (#installation)
+        2. [Usage] (#usage)
+        3. [Credits] (#credits)
+        4. [Tests] (#tests)
+
+        ## Installation
+
+        ${questions.installation}
+
+        ## Usage
+
+        ${questions.usage}
+
+        ## Screenshot
+
+        N/A
+
+        ## Credits
+
+        ${questions.credits}
+
+        ## License
+
+        N/A
+
+        ## Badges
+
+        N/A
+
+        ## Features
+
+        N/A
+
+        ## Contribution
+
+        N/A
+
+        ## Tests
+
+        ${questions.tests};
+    `
+    return inputData;
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let answers = generateReadMe(data)
+    fs.writeFile(fileName, answers, (err) =>
+    err ? console.log(err) : console.log("Success!"),)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    fileName = 'README.md',
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        console.log(data, 'answers'),
+        writeToFile(fileName, data)
+    }
+    )
+};
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# Professional README Generator Starter Code
-
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+# Title
+# ${questions.title}
+
+
+
+## Description
+
+${questions.description}
+
+## Table of Contents
+
+${questions.table-of-contents}
+
+## Installation
+
+${questions.installation}
+
+## Usage
+
+${questions.usage}
+
+## Screenshot
+
+${questions.screenshot}
+
+## Credits
+
+${questions.credits}
+
+## License
+
+${questions.license}
+
+## Badges
+
+${questions.badges}
+
+## Features
+
+${questions.features}
+
+## Contribution
+
+${questions.contribution}
+
+## Tests
+
+${questions.tests}
